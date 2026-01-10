8bc3f75b6bb87f6f59a7da7114391110daf3f3f6
Wiktor Wilński <wilinskiwiktor27@gmail.com>
2026-01-10 19:42:25 +0100

    General exception handler

---
+++ src/main/java/com/task/atiperatask/api/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.task.atiperatask.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<Map<String, Object>> handleHttpClientErrorException(HttpClientErrorException ex) {
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("status", HttpStatus.NOT_FOUND.value());
+        body.put("message", ex.getResponseBodyAsString());
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+}
